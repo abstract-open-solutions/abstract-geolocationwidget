e3d3e92defff2da7e8e1dd94ef11012d66c0470b
lucas <lucas@e34d31df-1f0d-4730-8d99-dbf2ec4dd0dd>
2009-09-03 12:57:06 +0000

    fix multi google map keys

--- abstract/geolocationwidget/skins/geolocation_templates/maps.py
+++ abstract/geolocationwidget/skins/geolocation_templates/maps.py
@@ -2,13 +2,14 @@ from Products.CMFCore.utils import getToolByName
 
 root = getToolByName(context, 'portal_url')
 root = root.getPortalObject()
+
 google_key_d = root.getProperty("gmap_key", "no key")
+#key1=abcd,key2=abcd
 try:
-    print 'ciao'
-    google_keys = eval(google_key_d)
+    google_keys_arr = google_key_d.split(',')
+    google_keys = dict([entry.split('=') for entry in google_keys_arr])
     google_key = google_keys[root.absolute_url()]
 except:
-    print 'bla'
     google_key = google_key_d
     
 return google_key
